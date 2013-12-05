066017b9b891be5015d6609d9ff181fd0b5703ad
Luis Novo <excc0024@est.igae.minhap.es>
2013-12-05 13:14:17 +0100

    Added H2 test database, dbunit

--- src/test/java/net/mikasa/mikasaweb/GenericTest.java
+++ src/test/java/net/mikasa/mikasaweb/GenericTest.java
@@ -1,15 +1,21 @@
 package net.mikasa.mikasaweb;
 
-
-import javax.sql.DataSource;
-
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-context.xml")
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,DirtiesContextTestExecutionListener.class,TransactionalTestExecutionListener.class,DbUnitTestExecutionListener.class })
+@DatabaseSetup("/net/mikasa/mikasaweb/DataTest.xml")
 public abstract class GenericTest {
 
 
