f159c15fcf373f71cbe8ccb9973df7d2cb71f081
Luis Novo <luis.novo@gmail.com>
2014-05-31 23:30:14 +0200

    Some cleaning before taking out jsf

--- src/main/java/net/mikasa/mikasaweb/controller/HomeController.java
+++ src/main/java/net/mikasa/mikasaweb/controller/HomeController.java
@@ -1,10 +1,7 @@
 package net.mikasa.mikasaweb.controller;
 
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.security.access.annotation.Secured;
 
 @Controller
 public class HomeController
