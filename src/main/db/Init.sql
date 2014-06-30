af2dd5024635414e67ab1e2f7b1c9fdb1635a508
Luis Novo <lnovo@atsistemas.com>
2014-06-30 16:51:57 +0200

    Cloudbees ddbb config

--- src/main/db/Init.sql
+++ src/main/db/Init.sql
@@ -1,3 +1,4 @@
+-- MySQL
 drop database mikasadb;
 drop user 'mikasadb'@'localhost';
 create database mikasadb;
@@ -5,3 +6,11 @@ create user 'mikasadb'@'localhost' identified by 'mikasadb';
 grant all privileges on mikasadb.* to mikasadb;
 flush privileges;
 commit;
+
+-- Oracle
+/*
+drop user mikasadb cascade;
+create user mikasadb identified by mikasadb;
+grant all privileges to mikasadb;
+commit;
+*/
