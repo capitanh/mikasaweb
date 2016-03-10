df5bc502c56376a6893ad7c635ec18914258e0a8
capitanh <luis.novo@gmail.com>
2016-03-10 10:44:42 +0100

    Updating repo before new improvements

--- src/main/java/net/mikasa/mikasaweb/controller/UploadController.java
+++ src/main/java/net/mikasa/mikasaweb/controller/UploadController.java
@@ -1,15 +1,20 @@
 package net.mikasa.mikasaweb.controller;
 
+import net.mikasa.mikasaweb.model.Execution;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/executions")
 public class UploadController {
 	
 	private static Logger log = LoggerFactory.getLogger(UploadController.class);
@@ -26,5 +31,13 @@ public class UploadController {
 		log.info("File upload request received: " + file.getName());
 		return ("OK");
     }
+	
+	@RequestMapping(value="/streams",method=RequestMethod.PUT)
+	@ResponseBody
+	public String updateExecutions(@RequestPart("file") MultipartFile multipartFile, @RequestBody Execution execution)
+	{
+		log.info("Update Execution request received: " + execution.getName());
+		return "OK";
+	}
 
 }
