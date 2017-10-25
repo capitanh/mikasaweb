c4ada7ac4a42038fd9e613be53868829c3a4432f
luis.novo <luis.novo@gmail.com>
2017-10-26 00:55:47 +0200

    Added wildfly role

--- src/integration/java/net/mikasa/mikasaweb/it/ValidUserIT.java
+++ src/integration/java/net/mikasa/mikasaweb/it/ValidUserIT.java
@@ -16,7 +16,7 @@ public class ValidUserIT {
   public void setUp() throws Exception {
     //driver = new FirefoxDriver();
     driver = new HtmlUnitDriver();
-    baseUrl = "http://localhost:8080/mikasaweb";
+    baseUrl = "http://localhost:8888/mikasaweb";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
@@ -29,7 +29,7 @@ public class ValidUserIT {
     driver.findElement(By.id("password")).sendKeys("admin");
     driver.findElement(By.id("login")).click();
     assertTrue(driver.getPageSource().contains("mikasa.net 4.0 beta. Welcome"));
-    
+
   }
 
   @After
