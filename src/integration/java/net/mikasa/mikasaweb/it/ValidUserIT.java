671faba9fe5ac48d14b5cbc21d141760ae877bbe
luis.novo <luis.novo@gmail.com>
2017-11-02 00:14:16 +0100

    Getting close to spring boot web testing with selenium

--- src/integration/java/net/mikasa/mikasaweb/it/ValidUserIT.java
+++ src/integration/java/net/mikasa/mikasaweb/it/ValidUserIT.java
@@ -2,12 +2,30 @@ package net.mikasa.mikasaweb.it;
 
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
-import static org.junit.Assert.*;
-import org.openqa.selenium.*;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+import static org.junit.Assert.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ValidUserIT {
+  
+  @LocalServerPort
+  private int port;
+  
   private WebDriver driver;
   private String baseUrl;
   private StringBuffer verificationErrors = new StringBuffer();
@@ -16,7 +34,8 @@ public class ValidUserIT {
   public void setUp() throws Exception {
     //driver = new FirefoxDriver();
     driver = new HtmlUnitDriver();
-    baseUrl = "http://localhost:8888/mikasaweb";
+    //baseUrl = "http://localhost:" + port + "/mikasaweb";
+    baseUrl = "http://localhost:" + port;
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
