f114a1e14fe0ca2bb424314ff2024ba20487d056
luis.novo <luis.novo@gmail.com>
2017-11-25 12:10:05 +0100

    Finally, Selenium functional tests within Spring boot

--- src/test/java/net/mikasa/mikasaweb/it/ValidUserIT.java
+++ src/test/java/net/mikasa/mikasaweb/it/ValidUserIT.java
@@ -1,13 +1,11 @@
 package net.mikasa.mikasaweb.it;
 
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -28,11 +26,8 @@ public class ValidUserIT {
 
   @Before
   public void setUp() throws Exception {
-    //driver = new FirefoxDriver();
-    driver = new HtmlUnitDriver();
-    baseUrl = "http://localhost:" + port + "/mikasaweb";
-    //baseUrl = "http://localhost:" + port;
-    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver = new HtmlUnitDriver(true);
+    baseUrl = "http://localhost:" + port;
   }
   
   @Test
