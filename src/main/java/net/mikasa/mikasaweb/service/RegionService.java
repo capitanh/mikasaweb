c6f765d8a5733c834b6f669842d7fa64ac08ca07
luis.novo <luis.novo@gmail.com>
2017-11-26 22:31:40 +0100

    Fixed component scan path

--- src/main/java/net/mikasa/mikasaweb/service/RegionService.java
+++ src/main/java/net/mikasa/mikasaweb/service/RegionService.java
@@ -3,18 +3,19 @@ package net.mikasa.mikasaweb.service;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.mikasa.mikasaweb.dao.RegionDAO;
 import net.mikasa.mikasaweb.model.Region;
 
-//@Service
+@Service
+@Transactional
 public class RegionService {
-  
+
   @Autowired
   private RegionDAO regionDAO;
-  
+
   public Region findById(int id)
   {
     return regionDAO.findOne(new BigDecimal(id));
--- src/test/java/net/mikasa/mikasaweb/config/TestConfiguration.java
+++ src/test/java/net/mikasa/mikasaweb/config/TestConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -15,25 +16,26 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories("net.mikasa.mikasaweb.dao")
+@ComponentScan(basePackages = "net.mikasa.mikasaweb")
+@EnableJpaRepositories("net.mikasa.mikasaweb")
 @EnableTransactionManagement
 public class TestConfiguration {
-	
+
 	@Bean
   public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
 	}
-	
+
 	@Bean
   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
     entityManagerFactoryBean.setDataSource(dataSource());
     entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    entityManagerFactoryBean.setPackagesToScan("net.mikasa.mikasaweb.model");
+    entityManagerFactoryBean.setPackagesToScan("net.mikasa.mikasaweb");
     entityManagerFactoryBean.setJpaProperties(jpaProperties());
     return entityManagerFactoryBean;
   }
-	
+
 	private Properties jpaProperties() {
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.hbm2ddl.auto","create");
@@ -42,13 +44,13 @@ public class TestConfiguration {
 		properties.setProperty("hibernate.format_sql","false");
 		return properties;
   }
-	
+
 	@Bean
   public JpaTransactionManager transactionManager() {
     JpaTransactionManager transactionManager = new JpaTransactionManager();
     transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
     transactionManager.setDataSource(dataSource());
     return transactionManager;
-  }	
-   
+  }
+
 }
