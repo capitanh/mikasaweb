704ec2fa3ff1ec8396760d439afe2df9c58892a1
capitanh <luis.novo@gmail.com>
2017-11-29 14:35:28 +0100

    Unit and integration test separation

---
+++ src/integrationTest/resources/import.sql
@@ -0,0 +1,10 @@
+insert into users (username,password,enabled) values ('admin','admin',1);
+insert into authorities(username,authority) values ('admin','administrator');
+
+INSERT INTO regions VALUES (1,'Andalucía');
+INSERT INTO regions VALUES (2,'Aragón');
+INSERT INTO regions VALUES (3,'Principado de Asturias');
+INSERT INTO regions VALUES (4,'Illes Balears');
+INSERT INTO regions VALUES (5,'Cantabria');
+
+commit;
