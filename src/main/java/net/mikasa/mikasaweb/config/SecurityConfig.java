532597720adadfd64fb4dee183bdd8070dd65aed
luis.novo <luis.novo@gmail.com>
2017-12-13 01:00:34 +0100

    LDAP Authentication

--- src/main/java/net/mikasa/mikasaweb/config/SecurityConfig.java
+++ src/main/java/net/mikasa/mikasaweb/config/SecurityConfig.java
@@ -1,26 +1,43 @@
 package net.mikasa.mikasaweb.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.jdbcAuthentication()
-		.dataSource(dataSource);
-	}
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+  
+  //Get config from application.properties
+  @Value("${ldap.url}")
+  private String ldapUrl;
+  
+  @Value("${ldap.basedn}")
+  private String ldapBaseDn;
+  
+  @Value("${ldap.username}")
+  private String ldapUserName;
+  
+  @Value("${ldap.password}")
+  private String ldapPassword;
+  
+  //@Value("${ldap.user.dn.pattern}")
+  //private String ldapUserDnPattern;
+  
+  
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    
+    auth.ldapAuthentication()
+      .contextSource().url(ldapUrl + ldapBaseDn)
+      .managerDn(ldapUserName)
+      .managerPassword(ldapPassword)
+      .and()
+      .userSearchBase("ou=people")
+      .userSearchFilter("(uid={0})");    
+  }
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
