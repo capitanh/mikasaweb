2f75e6b55bd97ca34220073b1e3dc132868be46c
luis.novo <luis.novo@gmail.com>
2017-12-14 23:15:24 +0100

    Integration tests. Stll need to find a way to disable embbeded mongo

--- src/integrationTest/java/net/mikasa/mikasaweb/config/ITGenericTest.java
+++ src/integrationTest/java/net/mikasa/mikasaweb/config/ITGenericTest.java
@@ -1,10 +1,15 @@
 package net.mikasa.mikasaweb.config;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ITTestConfiguration.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = ITTestConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ITGenericTest {
 }
--- src/integrationTest/java/net/mikasa/mikasaweb/config/ITTestConfiguration.java
+++ src/integrationTest/java/net/mikasa/mikasaweb/config/ITTestConfiguration.java
@@ -6,13 +6,17 @@ import javax.sql.DataSource;
 
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
+@Configuration
+//@EnableAutoConfiguration(exclude={EmbeddedMongoAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ComponentScan("net.mikasa.mikasaweb")
 public class ITTestConfiguration {
       
